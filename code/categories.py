ed5950a86a98299cd6a72fa016c87beee1e64704
KAFKA <137051370+KAFKA2306@users.noreply.github.com>
2023-10-28 20:16:41 +0900

    Update categories.py

--- code/categories.py
+++ code/categories.py
@@ -1,41 +1,52 @@
-# CATEGORIES = {
-#     # Geographic Markets
-#     'US Stocks': ['MSFT', 'TSLA','AAPL', 'GOOGL', 'AMZN', 'BRK-B', 'PLTR', 'DIS', 'F', 'MARA', 'MSTR', 'Z', 'META',  'MOS', 'LYFT', 'DLO','COIN'],
-#     'Japanese Stocks': ['7203.T', '9984.T', '6758.T', '9433.T', '8306.T', '2621.T', '2511.T', '2647.T', '2838.T', '2512.T'],
-#     'European Stocks': ['ADS.DE', 'ALV.DE', 'BAS.DE', 'BAYN.DE'],
-#     'China Stocks and ETFs': ['CWEB', 'YINN', 'CXSE'],
 
-#     # Industry Specific
-#     'Healthcare': ['JNJ', 'PFE', 'MRK', 'UNH', 'ABBV', 'ABC', 'TDOC'],
-#     'Financial Stocks': ['JPM', 'BAC', 'WFC', 'C', 'GS'],
-#     'Consumer Goods': ['PG', 'KO', 'PEP', 'MCD', 'WMT'],
+FRED_API_KEY = 'yours'
 
-#     # Asset Classes
-#     'Bonds': ['TLT', 'IEF', 'SHY', 'AGG', 'LQD', 'BND', 'VGLT', 'VGSH', 'BSV', 'BIV', 'BLV', 'BNDX', 'TMF'],
-#     'Goods and Commodities': ['GLD', 'SLV', 'USO', 'DBA', 'DBC', 'GLDM', 'BLOK', 'GRES'],
-#     'US ETFs': ['SPHQ', 'VTI', 'VOO', 'VIG', 'VT', 'QQQ', 'VWO', 'VYM', 'TQQQ', 'UPRO', 'VDE', 'VDC', 'VHT', 'EDV','VXUS', 'CURE','SOXL','TMV'],
-# }
+# File Paths
+DATA_DIR = "./../data/"
+# Define constants for file paths
+FILE_PATHS = {
+    'stock': "M:\\ML\\Finance\\BI\\data\\stock.pkl",
+    'fred': "M:\\ML\\Finance\\BI\\data\\fred.pkl",
+    'stock_fred_pkl': "M:\\ML\\Finance\\BI\\data\\stock_fred.pkl",
+    'stock_fred_csv': "M:\\ML\\Finance\\BI\\data\\stock_fred.csv"
+}
+
+# yfinance
+ETF_CATEGORIES = [
+    'VOO', 'VIG',  # Standard ETFs
+    'TLT', 'IEF', 'AGG',  # Bonds
+    'FXE', 'FXY', 'FXB', 'FXA',  # Forex
+    'GLD',  # Goods and Commodities
+    'TQQQ', 'UPRO', 'TMF', 'TMV', 'CURE', 'SOXL',  # Leveraged ETFs
+    'CWEB', 'YINN',  # Country-specific ETFs
+    'JPY=X',  # USD/JPY
+    'EURJPY=X'  # EUR/JPY
+]
+
+
+INDIVIDUAL_STOCKS = [
+    'MSFT', 'AAPL', 'AMZN', 'GOOGL', 'BRK-B', 'JPM',  # US Stocks
+    'JNJ', 'PFE', 'MRK', 'UNH',  # Healthcare
+    'PG', 'KO', 'PEP', 'MCD',  # Consumer Goods
+    'COIN'  # Crypto Related
+]
 
 CATEGORIES = {
-    # Geographic Markets
-    'US Stocks': ['MSFT', 'AAPL', 'AMZN', 'GOOGL', 'BRK-B', 'JPM'],
-    'Japanese Stocks': ['7203.T', '9984.T', '6758.T', '8306.T'],
-    'European Stocks': ['ADS.DE', 'ALV.DE', 'BAS.DE'],
-    'China Stocks and ETFs': ['CWEB', 'YINN'],
+    'ETF': ETF_CATEGORIES,
+    'Individual Stocks': INDIVIDUAL_STOCKS
+}
 
-    # Industry Specific
-    'Tech Stocks': ['MSFT', 'AAPL', 'GOOGL', 'AMZN'],
-    'Healthcare': ['JNJ', 'PFE', 'MRK', 'UNH'],
-    'Financial Stocks': ['JPM', 'BAC', 'WFC'],
-    'Consumer Goods': ['PG', 'KO', 'PEP', 'MCD'],
+# Economic Indicators
+ECONOMIC_INDICATORS = ['GDP', 'CPIAUCNS', 'UNRATE', 'PAYEMS', 'INDPRO', 'CIVPART', 'HOUST']
 
-    # Asset Classes
-    'Bonds': ['TLT', 'IEF', 'AGG'],
-    'Goods and Commodities': ['GLD', 'SLV', 'USO'],
-    'Standard ETFs': ['SPHQ', 'VTI', 'VOO', 'VIG'],
+# Financial Markets
+FINANCIAL_MARKETS = ['FEDFUNDS', 'GS10', 'GS1', 'AAA', 'BAA', 'SP500', 'NASDAQCOM', 'DJIA', 'WILL5000INDFC', 'VIXCLS', 'TEDRATE']
 
-    # Special Categories
-    'Leveraged ETFs': ['TQQQ', 'UPRO', 'TMF', 'TMV', 'CURE', 'SOXL'],
-    'Crypto Related': ['BTCUSD', 'COIN'],
-    'Forex': ['EURUSD', 'GBPUSD', 'USDJPY', 'AUDUSD'],
+# Exchange and Commodities
+EXCHANGE_AND_COMMODITIES = ['DTB3', 'DTB6', 'DTB1YR', 'AAA10Y', 'BAA10Y', 'MORTGAGE30US', 'MORTGAGE15US', 'WTISPLC', 'GASREGW', 'CPILFESL', 'PCE', 'CUSR0000SAC', 'RRSFS']
+
+FRED_CATEGORIES = {
+    'Economic Indicators': ECONOMIC_INDICATORS,
+    'Financial Markets': FINANCIAL_MARKETS,
+    'Exchange and Commodities': EXCHANGE_AND_COMMODITIES
 }
